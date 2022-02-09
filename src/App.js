4f2c5ebddd388ddf17a9c7098ea5a971a13ed15a
khanh bui <bmkhanh912@gmail.com>
2022-02-09 15:08:17 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bui minh khanh
+          bui minh khanh pham thu giang
         </p>
         <a
           className="App-link"
